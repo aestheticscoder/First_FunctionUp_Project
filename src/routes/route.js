534ec5418ea9114f379b26b16d2b5679b03c401c
aestheticscoder <believe0256@gmail.com>
2023-04-29 03:52:59 +0530

    switching branches

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,8 @@ const UserModel= require("../models/userModel.js")
 const bookModel = require("..//models/bookModel.js")
 const UserController= require("../controllers/userController")
 const BookController = require("../controllers/bookController.js")
+const newBookController = require("../controllers/newBookController.js")
+const newBookModel = require("../models/newBookModel.js")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -17,6 +19,13 @@ router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook)
 router.get("/getBooksData", BookController.getBooksData)
+
+router.post('/createNewBook', newBookController.createBook);
+router.get('/bookList', newBookController.bookList);
+router.post('/getBooksInYear', newBookController.getBooksInYear);
+router.post('/getParticularBooks', newBookController.getParticularBooks);
+router.get('/getXINRBooks', newBookController.getXINRBooks);
+router.get('/getRandomBooks', newBookController.getRandomBooks);
 
 
 
