1bd5a3b1eb7f1c89aa16483bfd8a47f40a3257ed
aestheticscoder <believe0256@gmail.com>
2023-05-02 22:23:56 +0530

    switch

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-    bookName: { type: String, required: true },
-    authorName: { type: String, required: true },
-    category: { type: String, required: true },
-    year: { type: Number, required: true }
-  });
-  
-  const Book = mongoose.model('Book', bookSchema);
-  
-  module.exports = mongoose.model('Book', bookSchema)
+  bookName: { type: String, required: true },
+  authorName: { type: String, required: true },
+  category: { type: String, required: true },
+  year: { type: Number, required: true },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,8 @@ const UserController= require("../controllers/userController")
 const BookController = require("../controllers/bookController.js")
 const newBookController = require("../controllers/newBookController.js")
 const newBookModel = require("../models/newBookModel.js")
+const booksAndAuthorController = require("../controllers/booksAndAuthorController.js")
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -27,6 +29,12 @@ router.post('/getParticularBooks', newBookController.getParticularBooks);
 router.get('/getXINRBooks', newBookController.getXINRBooks);
 router.get('/getRandomBooks', newBookController.getRandomBooks);
 
+
+// Without using Population and Reference
+router.get("/get-all-books", booksAndAuthorController.getAllBooks);
+router.post("/create-book", booksAndAuthorController.createBook);
+router.get("/get-all-authors", booksAndAuthorController.getAllAuthors);
+router.post("/create-author", booksAndAuthorController.createAuthor);
 
 
 module.exports = router;
